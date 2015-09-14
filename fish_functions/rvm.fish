6cea8c0ff598cbe2406dc0cf7186ef3a3908a828
Talha Mansoor <talha131@gmail.com>
2015-09-15 03:52:52 +0500

    Fish: Fix a bug in rvm fish function
    See for details
    
    https://github.com/lunks/fish-nuggets/pull/16
    https://stackoverflow.com/questions/32529350/gem-installed-in-global-gemset-is-not-available-in-other-gemsets/

--- fish_functions/rvm.fish
+++ fish_functions/rvm.fish
@@ -5,6 +5,9 @@ function rvm --description='Ruby enVironment Manager'
 
   # apply rvm_* and *PATH variables from the captured environment
   and eval (grep '^rvm\|^[^=]*PATH\|^GEM_HOME' $env_file | grep -v '_clr=' | sed '/^[^=]*PATH/s/:/" "/g; s/^/set -xg /; s/=/ "/; s/$/" ;/; s/(//; s/)//')
+  # needed under fish >= 2.2.0
+  and set -xg GEM_PATH (echo $GEM_PATH | sed 's/ /:/g')
+
   # clean up
   rm -f $env_file
 end
