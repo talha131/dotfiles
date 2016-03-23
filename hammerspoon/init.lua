c66b4daa2a5f3c9cddda03981bd5597168798530
Talha Mansoor <talha131@gmail.com>
2016-03-24 01:11:43 +0500

    Hammerspoon: Move reload config code to separate file

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,27 +2,11 @@
 
 local hyper = { "cmd", "alt", "shift", "ctrl" }
 
--- Reload Configuration
---- http://www.hammerspoon.org/go/#fancyreload
-function reloadConfig(files)
-    doReload = false
-    for _, file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
-end
-
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
-hs.alert.show("Config loaded")
-
 hs.hotkey.bind(hyper, 'Q', function() hs.caffeinate.lockScreen() end)
 
 -- Launch applications
 hs.hotkey.bind(hyper, '0', function() hs.application.launchOrFocus("iterm") end)
 
+require "reload-config"
 require "caffeine"
 require "clipboard"
