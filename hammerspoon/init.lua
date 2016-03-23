d3eababb2bcd2d46b61b1051da822fc0e2cb9a1d
Talha Mansoor <talha131@gmail.com>
2016-03-24 01:06:16 +0500

    Hammerspoon: Move Caffeine code to separate file

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -19,30 +19,10 @@ end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
 
--- Caffeine replacement
-local caffeine = hs.menubar.new()
-function setCaffeineDisplay(state)
-    if state then
-        caffeine:setIcon("caffeine-on.pdf")
-        caffeine:setTooltip("Awake - machine will refuse to sleep")
-    else
-        caffeine:setIcon("caffeine-off.pdf")
-        caffeine:setTooltip("Sleepy - machine is allowed to sleep")
-    end
-end
-
-function caffeineClicked()
-    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
-end
-
-if caffeine then
-    caffeine:setClickCallback(caffeineClicked)
-    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
-end
-
 hs.hotkey.bind(hyper, 'Q', function() hs.caffeinate.lockScreen() end)
 
 -- Launch applications
 hs.hotkey.bind(hyper, '0', function() hs.application.launchOrFocus("iterm") end)
 
+require "caffeine"
 require "clipboard"
