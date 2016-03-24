8204180eafdcbdb255bf2b73179fb4ad04e90e45
Talha Mansoor <talha131@gmail.com>
2016-03-24 23:02:21 +0500

    Hammerspoon: Hyper+W to sleep system

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -8,6 +8,8 @@ require "clipboard"
 
 -- Lock System
 hs.hotkey.bind(hyper, 'Q', function() hs.caffeinate.lockScreen() end)
+-- Sleep system
+hs.hotkey.bind(hyper, 'W', function() hs.caffeinate.systemSleep() end)
 
 -- Launch applications
 hs.hotkey.bind(hyper, '0', function() hs.application.launchOrFocus("iterm") end)
