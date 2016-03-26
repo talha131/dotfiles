b53e0eb05dd814c219971c4774a9f68a7a7be859
Talha Mansoor <talha131@gmail.com>
2016-03-27 04:58:50 +0500

    Hammerspoon: Refactor launch application code

--- hammerspoon/launch-applications.lua
+++ hammerspoon/launch-applications.lua
@@ -3,38 +3,52 @@
 local modalKey = hs.hotkey.modal.new(hyper, 'A', 'Launch Application  mode')
 modalKey:bind('', 'escape', function() modalKey:exit() end)
 
+local appShortCuts = {
+
+    D = 'Dash',
+    E = 'evernote',
+    F = 'firefox',
+    G = 'Github Desktop',
+    I = 'iterm',
+    O = 'Microsoft Onenote',
+    S = 'slack',
+    V = 'MacVim',
+}
+
+for key, app in pairs(appShortCuts) do
+
+    modalKey:bind('', key, function() hs.application.launchOrFocus(app) end, function() modalKey:exit() end)
+end
+
+
+local chromeAppShortCuts = {
+
+    H = {id = 'oppompclolbipehgmpngoaalfhfdpdhl',
+    title = 'Habits'},
+
+    X = {id = 'ggohgaegpbmbmmmoobmogjfjopncbnfb',
+    title = 'Expenses'}
+}
+
 function launchOrFocusChromeApp(key)
 
-    local apps = {}
-    apps['H'] = {id = 'oppompclolbipehgmpngoaalfhfdpdhl',
-              title = 'Habits'}
-    apps['X'] = {id = 'ggohgaegpbmbmmmoobmogjfjopncbnfb',
-              title = 'Expenses'}
-
-    if key ~= 'H' and key ~= 'X' then
+    if chromeAppShortCuts[key] == nil then
 
         return
     end
 
-    local appWindow = hs.window.find(apps[key].title)
+    local appWindow = hs.window.find(chromeAppShortCuts[key].title)
     if appWindow ~= nil then
 
         appWindow:focus()
     else
 
-        hs.execute('/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --app-id=' .. apps[key].id)
+        hs.execute('/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --app-id=' .. chromeAppShortCuts[key].id)
     end
 end
 
-modalKey:bind('', 'D', function() hs.application.launchOrFocus('Dash') end, function() modalKey:exit() end)
-modalKey:bind('', 'E', function() hs.application.launchOrFocus('evernote') end, function() modalKey:exit() end)
-modalKey:bind('', 'F', function() hs.application.launchOrFocus('firefox') end, function() modalKey:exit() end)
-modalKey:bind('', 'G', function() hs.application.launchOrFocus('Github Desktop') end, function() modalKey:exit() end)
-modalKey:bind('', 'H', function() launchOrFocusChromeApp('H') end, function() modalKey:exit() end)
-modalKey:bind('', 'I', function() hs.application.launchOrFocus('iterm') end, function() modalKey:exit() end)
-modalKey:bind('', 'O', function() hs.application.launchOrFocus('Microsoft Onenote') end, function() modalKey:exit() end)
-modalKey:bind('', 'S', function() hs.application.launchOrFocus('slack') end, function() modalKey:exit() end)
-modalKey:bind('', 'V', function() hs.application.launchOrFocus('MacVim') end, function() modalKey:exit() end)
-modalKey:bind('', 'X', function() launchOrFocusChromeApp('X') end, function() modalKey:exit() end)
+for key in pairs(chromeAppShortCuts) do
 
--- modalKey:bind('', '1', function() hs.application.launchOrFocus('') end, function() modalKey:exit() end)
+    modalKey:bind('', key, function() launchOrFocusChromeApp(key) end, function() modalKey:exit() end)
+end
+
