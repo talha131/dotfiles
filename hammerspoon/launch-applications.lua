6100409fda8a15920365a88d64367f6772d3ee71
Talha Mansoor <talha131@gmail.com>
2016-04-11 05:26:21 +0500

    Hammerspoon: Move launch chrome applications code to separate file

--- hammerspoon/launch-applications.lua
+++ hammerspoon/launch-applications.lua
@@ -23,35 +23,3 @@ for key, app in pairs(appShortCuts) do
     modalKey:bind('', key, 'Launching '..app, function() hs.application.launchOrFocus(app) end, function() modalKey:exit() end)
 end
 
-
-local chromeAppShortCuts = {
-
-    H = {id = 'oppompclolbipehgmpngoaalfhfdpdhl',
-    title = 'Habits'},
-
-    X = {id = 'ggohgaegpbmbmmmoobmogjfjopncbnfb',
-    title = 'Expenses'}
-}
-
-function launchOrFocusChromeApp(key)
-
-    if chromeAppShortCuts[key] == nil then
-
-        return
-    end
-
-    local appWindow = hs.window.find(chromeAppShortCuts[key].title)
-    if appWindow ~= nil then
-
-        appWindow:focus()
-    else
-
-        hs.execute('/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --app-id=' .. chromeAppShortCuts[key].id)
-    end
-end
-
-for key in pairs(chromeAppShortCuts) do
-
-    modalKey:bind('', key, function() launchOrFocusChromeApp(key) end, function() modalKey:exit() end)
-end
-
