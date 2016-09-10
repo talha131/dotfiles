1b4618202fde116a7c0c528426683b969e56d08b
Talha Mansoor <talha131@gmail.com>
2016-09-10 11:57:51 +0500

    Fish: Add my own update method
    It updates brew, pip packages, gem, vim, npm and completions

--- fish/fish_functions/up.fish
+++ fish/fish_functions/up.fish
@@ -1,56 +1,56 @@
-# up: https://github.com/justinmayer/tackle/tree/master/plugins/up
 function up -d "Update software to the latest versions"
-    if contains "all" $argv
-        which brew >/dev/null
-        and begin
-            brew update
-            brew upgrade --all
-        end
-        which tlmgr >/dev/null
-        and  tlmgr update --self --all
-        set -l plugins python vundle
-        for plugin in $plugins
-            if contains $plugin $tacklebox_plugins
-                up $plugin
-            end
-        end
-        fish_update_completions
-    else
-        for arg in $argv
-            if contains $arg $tacklebox_plugins
-                switch $arg
-                    case "python"
-                        if test -z $VIRTUAL_ENV
-                            set -l os (uname)
-                            if test "$os" = "Linux"
-                                set -l sudo "sudo"
-                            else
-                                set -l sudo ""
-                            end
-                            set -l outdated (env PIP_REQUIRE_VIRTUALENV="" pip list --outdated | cut -d ' ' -f 1)
-                            for pkg in $outdated
-                                set python_packages_to_upgrade $python_packages_to_upgrade $pkg
-                            end
-                            if test -z "$python_packages_to_upgrade"
-                                echo "Python packages to up-to-date."
-                            else
-                                env PIP_REQUIRE_VIRTUALENV="" $sudo pip install --upgrade $python_packages_to_upgrade
-                                set -e python_packages_to_upgrade
-                            end
-                        else
-                            if test -f requirements.txt
-                                pip install --upgrade -r requirements.txt
-                            else
-                                set -l outdated (pip list --outdated | cut -d ' ' -f 1)
-                                pip install --upgrade $outdated
-                            end
-                        end
-                    case "vundle"
-                        env SHELL=/bin/bash vim +PluginInstall! +PluginClean +qall
-                end
-            else
-                echo "Could not locate that plugin in your tacklebox_plugins setting."
-            end
-        end
+
+    which brew >/dev/null
+    and begin
+        echo "Updating Brew"
+        brew update 
+        echo "Upgrading Brew"
+        brew reinstall --HEAD neovim
+        brew upgrade --all
+        echo "Cleanup Brew"
+        brew linkapps --local
+        brew cleanup
+        brew cask cleanup
     end
+
+    which pip > /dev/null
+    and begin
+        echo "Updating pip"
+        pip install --upgrade pip
+        echo "Updating pip packages"
+        pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs pip install -U
+    end
+
+    which gem > /dev/null
+    and begin
+        echo "Updating gems"
+        gem update (eval gem outdated | cut -d ' ' -f 1)
+        echo "Cleaning up old versions of Gem"
+        brew cleanup
+    end
+
+    which pyenv > /dev/null
+    and begin
+        echo "Rebuild pyenv shim binaries"
+        pyenv rehash
+    end
+
+    which pyenv > /dev/null
+    and begin
+        echo "Update Vim Plugins"
+        vim -i NONE -c VundleUpdate -c quitall
+    end
+
+    which npm > /dev/null
+    and begin
+        echo "Update NPM global packages"
+        npm outdated -g
+        npm update -g
+        npm outdated -g
+    end
+
+    echo "Updating completions"
+    fish_update_completions
+
 end
+
