e07095f1d40630dbcb8272ca320bed94d494994e
Talha Mansoor <talha131@gmail.com>
2016-09-15 12:17:32 +0500

    Fish: Load rvm wrapper

--- fish/config.fish
+++ fish/config.fish
@@ -3,3 +3,5 @@ eval (python -m virtualfish compat_aliases  auto_activation)
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
+
+rvm default
