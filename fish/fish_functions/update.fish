01ffb1769b7d5bf4152ab1e3dd20b1cd7dbcc31b
Talha Mansoor <talha131@gmail.com>
2016-09-16 17:16:51 +0500

    Fish: Move private methods inside update method
    This way private methods do not go out of scope as long as update
    function is running

--- fish/fish_functions/update.fish
+++ fish/fish_functions/update.fish
@@ -1,5 +1,110 @@
 function update -d "Update software to the latest versions"
 
+    function _printMessage
+        for arg in $argv
+            set_color -o yellow; echo "📌  "$arg
+        end
+        set_color normal
+    end
+
+    function _updateBrew
+        which brew >/dev/null
+        and begin
+            _printMessage "Updating Brew"
+            brew update
+        end
+        functions -e _updateBrew
+    end
+
+    function _updateBrewPackages
+        which brew >/dev/null
+        and begin
+            _printMessage "Upgrading Brew"
+            brew upgrade --all
+            brew linkapps
+            _printMessage "Cleanup Brew"
+            brew cleanup
+            brew cask cleanup
+        end
+        functions -e _updateBrewPackages
+    end
+
+    function _updateNeoVim
+        which brew >/dev/null
+            and begin
+            _printMessage "Updating NeoVim"
+            brew reinstall --HEAD neovim
+        end
+        functions -e _updateNeoVim
+    end
+
+    function _updatePipPackages
+        which pip > /dev/null
+        and begin
+            _printMessage "Updating pip"
+            pip install --upgrade pip
+            _printMessage "Updating pip packages"
+            pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs pip install -U
+        end
+        functions -e _updatePip
+    end
+
+    function _updateGems
+        which gem > /dev/null
+        and begin
+            _printMessage "Updating gems"
+            gem update (eval gem outdated | cut -d ' ' -f 1)
+            _printMessage "Cleaning up old versions of Gem"
+            gem cleanup
+        end
+        functions -e _updateGems
+    end
+
+    function _updateRbenvShims
+        which rbenv > /dev/null
+        and begin
+            _printMessage "Rebuild rbenv shim"
+            rbenv rehash
+        end
+        functions -e _updateRbenvShims
+    end
+
+    function _updatePyenvShims
+        which pyenv > /dev/null
+        and begin
+            _printMessage "Rebuild pyenv shim binaries"
+            pyenv rehash
+        end
+        functions -e _updatePyenvShims
+    end
+
+    function _updateVimPlugins
+        which vim > /dev/null
+        and begin
+            _printMessage "Update Vim Plugins"
+            vim -i NONE -c PlugUpgrade -c PlugUpdate -c PlugClean! -c quitall
+        end
+        functions -e _updateVimPlugins
+    end
+
+    function _updateNpmPackages
+        which npm > /dev/null
+        and begin
+            _printMessage "Update NPM global packages"
+            npm outdated -g
+            npm update -g
+            npm outdated -g
+        end
+        functions -e _updateNpmPackages
+    end
+
+    function _updateFishCompletions
+        _printMessage "Updating completions"
+        fish_update_completions
+        functions -e _updateFishCompletions
+    end
+
+    # Main method starts from here
     set argument all brew fish gem npm nvim pip vim
     set validArgument "false"
 
@@ -63,6 +168,7 @@ function update -d "Update software to the latest versions"
         echo "🚩  "$argument
         set_color normal
     end
+
 end
 
 complete --no-files -c update -a all  -d 'Update all packages. Except nvim'
@@ -73,108 +179,4 @@ complete --no-files -c update -a npm  -d 'Update global npm packages'
 complete --no-files -c update -a nvim -d 'Install NeoVim from HEAD of its git repository'
 complete --no-files -c update -a pip  -d 'Update pip and installed packages. Update pyenv shims'
 complete --no-files -c update -a vim  -d 'Update and clean up Vim plugins. Require vim-plug'
-
-function _printMessage
-    for arg in $argv
-        set_color -o yellow; echo "📌  "$arg
-    end
-    set_color normal
-end
-
-function _updateBrew
-    which brew >/dev/null
-    and begin
-        _printMessage "Updating Brew"
-        brew update
-    end
-    functions -e _updateBrew
-end
-
-function _updateBrewPackages
-    which brew >/dev/null
-    and begin
-        _printMessage "Upgrading Brew"
-        brew upgrade --all
-        brew linkapps
-        _printMessage "Cleanup Brew"
-        brew cleanup
-        brew cask cleanup
-    end
-    functions -e _updateBrewPackages
-end
-
-function _updateNeoVim
-    which brew >/dev/null
-        and begin
-        _printMessage "Updating NeoVim"
-        brew reinstall --HEAD neovim
-    end
-    functions -e _updateNeoVim
-end
-
-function _updatePipPackages
-    which pip > /dev/null
-    and begin
-        _printMessage "Updating pip"
-        pip install --upgrade pip
-        _printMessage "Updating pip packages"
-        pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs pip install -U
-    end
-    functions -e _updatePip
-end
-
-function _updateGems
-    which gem > /dev/null
-    and begin
-        _printMessage "Updating gems"
-        gem update (eval gem outdated | cut -d ' ' -f 1)
-        _printMessage "Cleaning up old versions of Gem"
-        gem cleanup
-    end
-    functions -e _updateGems
-end
-
-function _updateRbenvShims
-    which rbenv > /dev/null
-    and begin
-        _printMessage "Rebuild rbenv shim"
-        rbenv rehash
-    end
-    functions -e _updateRbenvShims
-end
-
-function _updatePyenvShims
-    which pyenv > /dev/null
-    and begin
-        _printMessage "Rebuild pyenv shim binaries"
-        pyenv rehash
-    end
-    functions -e _updatePyenvShims
-end
-
-function _updateVimPlugins
-    which vim > /dev/null
-    and begin
-        _printMessage "Update Vim Plugins"
-        vim -i NONE -c PlugUpgrade -c PlugUpdate -c PlugClean! -c quitall
-    end
-    functions -e _updateVimPlugins
-end
-
-function _updateNpmPackages
-    which npm > /dev/null
-    and begin
-        _printMessage "Update NPM global packages"
-        npm outdated -g
-        npm update -g
-        npm outdated -g
-    end
-    functions -e _updateNpmPackages
-end
-
-function _updateFishCompletions
-    _printMessage "Updating completions"
-    fish_update_completions
-    functions -e _updateFishCompletions
-end
 
