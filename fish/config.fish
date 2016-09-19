55330aadbfba423f19cb1e076207258c9e290395
Talha Mansoor <talha131@gmail.com>
2016-09-19 09:11:48 +0500

    Fish: Set variables to make pyenv work
    https://github.com/yyuu/pyenv/issues/32#issuecomment-21019171

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,11 @@
 source ~/.config/fish/functions/marks.fish
+
+set PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
+pyenv rehash
+
 eval (python -m virtualfish compat_aliases  auto_activation)
+
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
