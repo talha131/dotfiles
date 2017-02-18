739a05c1fad9842e792633cde90f463c53d45a7a
Talha Mansoor <talha131@gmail.com>
2017-02-18 05:54:48 +0500

    Fish: Remove wakatime

--- fish/fish_functions/fish_prompt.fish
+++ fish/fish_functions/fish_prompt.fish
@@ -120,6 +120,3 @@ if echo (pwd) | grep -qEi "^/Users/$USER/Repos/"
 else
     set  project "Terminal"
 end
-
-wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 > /dev/null&
-
