004d1800a7d4351b226ca6269f61c1ed00ca4a0e
Talha Mansoor <talha131@gmail.com>
2017-10-23 00:42:11 +0500

    Fish: Enable path of pyenv
    This will let us use pyenv properly.

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,8 @@
 source ~/.config/fish/functions/marks.fish
 
-# set PYENV_ROOT $HOME/.pyenv
-# set -x PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
-# pyenv rehash
+set PYENV_ROOT $HOME/.pyenv
+set -x PATH $PYENV_ROOT/shims $PYENV_ROOT/bin $PATH
+pyenv rehash
 
 # eval (python -m virtualfish compat_aliases  auto_activation)
 
