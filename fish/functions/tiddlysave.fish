b51018bb64956a717dbb9a8fb701ca80ea49ee56
Talha Mansoor <talha131@gmail.com>
2018-07-06 02:10:37 +0500

    Fish: Add alias to make backup to tiddlywiki and then rename it to file modification time

---
+++ fish/functions/tiddlysave.fish
@@ -0,0 +1,23 @@
+function tiddlysave
+    set p /Users/talha/Dropbox/TiddlyNotes/output/
+    set f index.html
+
+    set file "$p""$f"
+    tiddlywiki ~/Dropbox/TiddlyNotes --verbose --build index
+
+    if test -e "$file"
+        set_color -o red
+        echo "index.html found"
+        
+        set append (stat -f "%Sm" -t "%F-%H%M" "$file")
+        set rootname (echo $f | sed 's/\.[^.]*$//')
+        set ext (echo $file | sed 's/.*\.//')
+        set new "$p""$rootname"-"$append"."$ext"
+
+        set_color -o yellow
+        echo "Rename $file → $new"
+        set_color normal
+
+        mv -v "$file" "$new"
+    end
+end
