17328c9ad44f8c47d24466c99c30628127f33d8a
Talha Mansoor <talha131@gmail.com>
2019-04-06 17:21:13 +0500

    Fish: Improve tiddlyserver command to support custom port and wiki
    It also uses tm_tiddly-next

--- fish/functions/tiddlyserver.fish
+++ fish/functions/tiddlyserver.fish
@@ -1,3 +1,30 @@
-function tiddlyserver
-    tiddlywiki ~/Dropbox/TiddlyNotes --listen root-tiddler=\$:/core/save/lazy-all
+function tiddlyserver -d 'Run tm_tiddly-next server'
+    # Default port
+    set _flag_p 8080
+    # My default tiddlywiki
+    set tw ~/Dropbox/TiddlyNotes
+    argparse --name=tiddlyserver 'h/help' 'p/port=' -- $argv
+
+    if test -n "$_flag_h"
+        echo 'Run tm_tiddly-next server'
+        printf '\n'
+        printf 'tiddlyserver wikiname --port [8000]\n'
+        printf '\n'
+        echo $tw' is the default tiddlywiki'
+        echo 'Default port is '$_flag_p
+        echo 'Use --port or -p to specify custom port'
+        printf '\n'
+        echo 'Example:'
+        echo 'tiddlyserver mynewwiki --port 8081'
+    else
+        # Check if wiki path is given
+        if test -n "$argv[1]"
+            set tw "$argv[1]"
+        end
+
+        echo 'Port:' $_flag_p
+        echo 'Tiddlywiki:' $tw
+
+        tm_tiddly-next $tw --listen port=$_flag_p root-tiddler=\$:/core/save/lazy-all
+    end
 end
