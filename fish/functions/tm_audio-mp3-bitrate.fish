b8774cc74727742f778735fe31effee206262ab3
Talha Mansoor <talha131@gmail.com>
2019-07-08 00:10:43 +0500

    fish: remove hard coded value

--- fish/functions/tm_audio-mp3-bitrate.fish
+++ fish/functions/tm_audio-mp3-bitrate.fish
@@ -3,7 +3,7 @@ function tm_audio-mp3-bitrate
         set name (echo "$f" | sed 's/\.[^.]*$//')
         set ext (echo "$f" | sed 's/.*\.//')
         if test $ext = "mp3"
-            set rate (ffprobe -v quiet -unit -prefix -print_format default  -select_streams a  -show_entries stream=bit_rate  046.mp3 | rg bit_rate | sed 's/bit_rate=//' | awk '{print $1}')
+            set rate (ffprobe -v quiet -unit -prefix -print_format default  -select_streams a  -show_entries stream=bit_rate  $f | rg bit_rate | sed 's/bit_rate=//' | awk '{print $1}')
             tm_printSuccess "$rate $f"
         else
             tm_printMessage "Skipping $f"
