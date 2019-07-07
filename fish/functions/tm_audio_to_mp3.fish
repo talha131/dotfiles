77041ad8c54d8872fe8f7cb5b590e169061a89b8
Talha Mansoor <talha131@gmail.com>
2019-07-07 19:43:50 +0500

    fish: add script to convert wav/mp3 to 32k mp3 in parallel

---
+++ fish/functions/tm_audio_to_mp3.fish
@@ -0,0 +1,32 @@
+function tm_audio_to_mp3 -d 'Convert wav/mp3 file to a lower bitrate mp3'
+
+    set rfile (gdate +"%Y-%m-%d %H%m%S %N.txt")
+    set dname (gdate +"%Y-%m-%d %H%m%S output")
+    for f in $argv
+        set name (echo "$f" | sed 's/\.[^.]*$//')
+        set ext (echo "$f" | sed 's/.*\.//')
+
+        if test -f $f
+            if test "$ext" = "mp3" || test "$ext" = "wav"
+                echo "ffmpeg -i \"$f\" -ac 1 -ar 44100 -b:a 32k \"$dname/$name.mp3\"" >> $rfile
+            end
+        end
+    end
+
+    set count (wc -l $rfile | awk '{print $1}')
+    if test $count != "0"
+        tm_printMessage "Convert $count files"
+        set start (gdate +%s)
+        mkdir $dname
+        tm_printMessage "Conversion started"
+        parallel --jobs 4 < $rfile
+        set end (gdate +%s)
+        set elappsed (math $end - $start)
+        tm_printMessage "Converted $count files in $elappsed seconds"
+        open $dname
+    end
+
+    tm_printMessage "Removing $rfile"
+    trash $rfile
+end
+
