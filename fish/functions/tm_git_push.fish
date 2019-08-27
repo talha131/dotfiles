3bc4b05ece7bbe13337a00ee586f667e2cb97991
Talha Mansoor <talha131@gmail.com>
2019-08-27 22:24:27 +0500

    fish: update tm_git_push to handle network down status

--- fish/functions/tm_git_push.fish
+++ fish/functions/tm_git_push.fish
@@ -1,14 +1,8 @@
 function tm_git_push -d 'Push to Git remote periodically'
     echo
     set attempt 1
-    while true
-       set a (git cherry -v)
-       if test $a
-           tm_printSuccess "Pushing new commits"
-           set attempt (math $attempt - 1)
-           git push
-           echo
-       else
+
+    function _wait
            echo -en "\e[1A"
            echo -n "Waiting... "
            for i in (seq 1 10)
@@ -16,7 +10,24 @@ function tm_git_push -d 'Push to Git remote periodically'
                echo -n "$i "
            end
            tm_printMessage "Attempt  $attempt  : No new commits"
+    end
+    
+    while true
+       set a (git cherry -v)
+       if test $a
+           tm_printSuccess "Pushing new commits"
+           set attempt (math $attempt - 1)
+           git push
+           or begin
+                  tm_printWarning "git push failed" 
+                  echo
+                  beep
+                  _wait
+              end
+           echo
+       else
+           _wait
+           set attempt (math $attempt + 1)
        end
-       set attempt (math $attempt + 1)
-   end
+    end
 end
