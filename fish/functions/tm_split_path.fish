04fccf5eb54577fb583f4d466447b466abfb47b5
Talha Mansoor <talha131@gmail.com>
2019-09-16 01:18:20 +0500

    fish: improve tm_split_path

--- fish/functions/tm_split_path.fish
+++ fish/functions/tm_split_path.fish
@@ -1,3 +1,12 @@
 function tm_split_path -d 'Return filename, ext, and directory from the path'
-    echo $argv[1] | gsed 's/\(.*\)\/\(.*\)\.\(.*\)$/\2\n\3\n\1/'
+    set result (echo $argv[1] | gsed -n 's/\(.*\)\/\(.*\)\.\(.*\)$/\2\n\3\n\1/p')
+    count $result > /dev/null
+    # If arg does have path like /path/filename.ext
+    and begin
+        string join \n $result
+    end
+    # If arg does not have path like filename.ext
+    or begin
+        echo $argv[1] | gsed -n 's/\(.*\)\.\(.*\)$/\1\n\2/p'
+    end
 end
