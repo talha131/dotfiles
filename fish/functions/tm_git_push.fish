cb8c944465a220554d369ade96f2afbb5ce7792d
Talha Mansoor <talha131@gmail.com>
2019-09-16 00:51:21 +0500

    fish: refactor and improve git push code

--- fish/functions/tm_git_push.fish
+++ fish/functions/tm_git_push.fish
@@ -1,6 +1,6 @@
 function tm_git_push -d 'Push to Git remote periodically'
     echo
-    set attempt 1
+    set -g attempt 1
 
     function _wait
            echo -en "\e[1A"
@@ -9,32 +9,40 @@ function tm_git_push -d 'Push to Git remote periodically'
                sleep 1
                echo -n "$i "
            end
-           tm_printMessage "Attempt  $attempt  : No new commits"
     end
     
     while true
-       set a (git cherry -v)
-       if test $status -ne 0
-           tm_printWarning "git cherry failed" 
-           echo
-           beep
-           _wait
-       else 
-           if test -n "$a"
-               tm_printSuccess "Pushing new commits"
-               set attempt (math $attempt - 1)
-               git push
+       git cherry > /dev/null 2> /dev/null
+       # git cherry is successful
+       and begin
+           # Check if there are unpushed commits 
+           count (git cherry) > /dev/null
+           # there are unpushed commits
+           and begin
+               git push > /dev/null 2> /dev/null
+               and begin 
+                   tm_printSuccess "Pushing new commits"
+               end
+               # git push failed
                or begin
-                   tm_printWarning "git push failed" 
-                   echo
-                   beep
                    _wait
+                   tm_printWarning "git push failed" 
+                   beepPreviousLine
                end
                echo
-           else
+           end
+           # there are no unpushed commits
+           or begin
                _wait
+               tm_printMessage " Attempt $attempt : No new commits"
                set attempt (math $attempt + 1)
            end
+       end
+       # git cherry fails
+       or begin
+           _wait
+           tm_printWarning "git cherry failed" 
+           beepPreviousLine
        end
     end
 end
