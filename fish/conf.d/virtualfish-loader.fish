f9a9693232ce86ed592308b54a5b360f1da19f3b
Talha Mansoor <talha131@gmail.com>
2020-04-18 13:18:27 +0500

    fish: auto generated settings

---
+++ fish/conf.d/virtualfish-loader.fish
@@ -0,0 +1,4 @@
+set -g VIRTUALFISH_VERSION 2.0.1
+set -g VIRTUALFISH_PYTHON_EXEC /Users/talha/.pyenv/versions/3.8.2/bin/python3.8
+source /Users/talha/.pyenv/versions/3.8.2/lib/python3.8/site-packages/virtualfish/virtual.fish
+emit virtualfish_did_setup_plugins
