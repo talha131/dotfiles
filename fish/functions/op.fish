e32a8657a3302bea7c37c6236faafb192e4cc0d9
Talha Mansoor <talha131@gmail.com>
2020-07-04 17:53:04 +0500

    fish: add comment

--- fish/functions/op.fish
+++ fish/functions/op.fish
@@ -1,3 +1,7 @@
 function op -d 'Open project layout in iTerm2'
+  # sed is required because itermocil appends .yml to filename sent to it via fzf
+  #
+  # 2> and > slients all the output from itermocil. It prints help message if no filename is passed to it.
+  # It happens when you do not select a file from fzf
   itermocil (ls ~/.itermocil/*.yml |  sed 's/\(.*\)\/\(.*\)\.\(.*\)$/\2/' | fzf) 2> /dev/null > /dev/null
 end
