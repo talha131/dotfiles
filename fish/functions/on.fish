d9755b372fdd504deef97564d5186af8c2848119
Talha Mansoor <talha131@gmail.com>
2020-08-01 23:51:01 +0500

    fish: add command to select a vim sessions

---
+++ fish/functions/on.fish
@@ -0,0 +1,10 @@
+function on -d 'Open a Neovim session'
+  set folder "/Users/talha/.vim/sessions"
+  set n (fd . -t f $folder | sed 's/.*\///' | choose)
+
+  if test -f "$folder/$n"
+    nvim -S "$folder/$n"
+  end
+
+end
+
