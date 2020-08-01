259e39f53e1db60d2db6aef943c201d1e1ebc590
Talha Mansoor <talha131@gmail.com>
2020-08-01 22:45:51 +0500

    fish: use choose instead of fzf
    choose shows the result in the middle of the screen

--- fish/functions/op.fish
+++ fish/functions/op.fish
@@ -3,5 +3,5 @@ function op -d 'Open project layout in iTerm2'
   #
   # 2> and > slients all the output from itermocil. It prints help message if no filename is passed to it.
   # It happens when you do not select a file from fzf
-  itermocil (ls ~/.itermocil/*.yml |  sed 's/\(.*\)\/\(.*\)\.\(.*\)$/\2/' | fzf) 2> /dev/null > /dev/null
+  itermocil (ls ~/.itermocil/*.yml |  sed 's/\(.*\)\/\(.*\)\.\(.*\)$/\2/' | choose) 2> /dev/null > /dev/null
 end
