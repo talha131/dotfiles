6a6b55132341a78ee22a35a54b752347b922cb0e
Talha Mansoor <talha131@gmail.com>
2020-10-18 16:52:35 +0500

    fish: confirm output of pcloud move

--- fish/functions/tm_bm_upload_audio.fish
+++ fish/functions/tm_bm_upload_audio.fish
@@ -35,15 +35,20 @@ function tm_bm_upload_audio -d 'Upload audio files from IC Recorder to pCloud'
             end
             tm_printMessage "Upload files to the pcloud"
             rclone move $t pcloud:"Project - Podcast Archives/A0 Friday Lectures - Raw Files - Todo" -P --delete-empty-src-dirs
-            test (ls $t)
             and begin
-                tm_printWarning "Directory $t is not empty"
-                open $t
+                test (ls $t)
+                and begin
+                    tm_printWarning "Directory $t is not empty"
+                    open $t
+                end
+                or begin
+                    tm_printMessage "Remove $t"
+                    trash $t
+                end
             end
-            or begin
-                tm_printMessage "Remove $t"
-                trash $t
-            end
+            or begin 
+                tm_printWarning "Failed to moves files to pcloud"
+            end 
         end
         or begin 
             tm_printWarning "Failed to create a temp folder"
