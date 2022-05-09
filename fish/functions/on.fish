4c7189bead269a7d1dbf3e6bf3682977bd955cbc
Talha <talha131@oncrashreboot.com>
2022-05-09 11:49:52 +0500

    fish: set path of on

--- fish/functions/on.fish
+++ fish/functions/on.fish
@@ -1,5 +1,5 @@
 function on -d 'Open a Neovim session'
-  set folder "/Users/talha/.vim/sessions"
+  set folder "/Users/talha/.local/share/nvim/sessions"
   set n (fd . -t f $folder | sed 's/.*\///' | choose)
 
   if test -f "$folder/$n"
