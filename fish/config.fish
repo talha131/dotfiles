1051ec314234540d1ce6b89aeda8289c3461e7de
Talha <talha131@oncrashreboot.com>
2022-05-09 12:36:25 +0500

    fish: remove invalid paths

--- fish/config.fish
+++ fish/config.fish
@@ -14,12 +14,6 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 # Set path
 ## Path of homebrew packages
 fish_add_path /opt/homebrew/bin
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -g fish_user_paths "$HOME/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/llvm/bin/" $fish_user_paths
-
-#Python path
-set -g fish_user_paths "/usr/local/opt/python/libexec/bin/" $fish_user_paths
 
 # ruby gem variables
 set -x GEM_HOME $HOME/.gem
