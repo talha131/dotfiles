2bf4b439514c3f6dc39f77a90d8444b2fa331c20
Talha <talha131@oncrashreboot.com>
2022-05-15 20:23:29 +0500

    hammerspoon: fix path of assets

--- hammerspoon/caffeine.lua
+++ hammerspoon/caffeine.lua
@@ -1,14 +1,18 @@
 -- Caffeine replacement
-local imagePath =  os.getenv("HOME") .. '/.hammerspoon/assets/';
-local iconAwake = imagePath .."caffeine-on.pdf"
-local iconSleep = imagePath .."caffeine-off.pdf"
+local HOME = os.getenv("HOME")
+local XDG_CONFIG_HOME = os.getenv("XDG_CONFIG_HOME") or HOME.."/.config"
+local HAMMERSPOON = XDG_CONFIG_HOME .. '/hammerspoon'
+
+local ASSETS_PATH =  HAMMERSPOON .. '/assets/';
+local iconAwake = ASSETS_PATH .."caffeine-on.pdf"
+local iconSleep = ASSETS_PATH .."caffeine-off.pdf"
 
 local caffeine = hs.menubar.new()
 
 function setCaffeineDisplay(state)
     if state then
         caffeine:setIcon(iconAwake)
-        caffeine:setTooltip(imagePath .."Awake - machine will refuse to sleep")
+        caffeine:setTooltip(ASSETS_PATH .."Awake - machine will refuse to sleep")
     else
         caffeine:setIcon(iconSleep)
         caffeine:setTooltip("Sleepy - machine is allowed to sleep")
