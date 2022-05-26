ad4b026f5184f2200a09526869ec46d87efb8357
Talha <talha131@oncrashreboot.com>
2022-05-26 12:00:51 +0500

    hammerspoon: add shortcuts to set volume

--- hammerspoon/audio-output.lua
+++ hammerspoon/audio-output.lua
@@ -9,6 +9,15 @@ function setVolume(v)
    end
 end
 
-modalKey:bind('', 'b', 'Set volume to 50%', function() setVolume(50) end, function() modalKey:exit() end)
+modalKey:bind('', '1', 'Set volume to 10%', function() setVolume(10) end, function() modalKey:exit() end)
+modalKey:bind('', '2', 'Set volume to 20%', function() setVolume(20) end, function() modalKey:exit() end)
+modalKey:bind('', '3', 'Set volume to 30%', function() setVolume(30) end, function() modalKey:exit() end)
+modalKey:bind('', '4', 'Set volume to 40%', function() setVolume(40) end, function() modalKey:exit() end)
+modalKey:bind('', '5', 'Set volume to 50%', function() setVolume(50) end, function() modalKey:exit() end)
+modalKey:bind('', '6', 'Set volume to 60%', function() setVolume(60) end, function() modalKey:exit() end)
+modalKey:bind('', '7', 'Set volume to 70%', function() setVolume(70) end, function() modalKey:exit() end)
+modalKey:bind('', '8', 'Set volume to 80%', function() setVolume(80) end, function() modalKey:exit() end)
+modalKey:bind('', '9', 'Set volume to 90%', function() setVolume(90) end, function() modalKey:exit() end)
+modalKey:bind('', '0', 'Set volume to 100%', function() setVolume(100) end, function() modalKey:exit() end)
 modalKey:bind('', 'm', 'Mute volume', function() setVolume(0) end, function() modalKey:exit() end)
 
