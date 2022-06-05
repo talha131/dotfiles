5dd25126fc0b137d9d58d2f1cb2159eec136a864
Talha <talha131@oncrashreboot.com>
2022-06-05 07:44:50 +0500

    fish: change the private function name space

--- fish/functions/pcloud-upload.fish
+++ fish/functions/pcloud-upload.fish
@@ -8,7 +8,7 @@ function pcloud-upload -d 'Upload file to fish'
     set count 0
     set limit 200
 
-    function __create_index_files -S
+    function _pcloud_upload_create_index_files -S
         if not test -d ~/.local/share/tm-pcloud
             tm_printMessage "Create files to store index and count"
             mkdir ~/.local/share/tm-pcloud
@@ -17,7 +17,7 @@ function pcloud-upload -d 'Upload file to fish'
         touch ~/.local/share/tm-pcloud/count
     end
 
-    function __set_index_files -S
+    function _pcloud_upload_set_index_files -S
         set cur (bat -p -r 1 ~/.local/share/tm-pcloud/current)
         if test -z $cur; or not test $cur -ge 0
             echo -n $i > ~/.local/share/tm-pcloud/current
@@ -27,18 +27,18 @@ function pcloud-upload -d 'Upload file to fish'
 
         set count (bat -p -r 1 ~/.local/share/tm-pcloud/count)
         if test -z $count; or not test $count -ge 0
-            __update_file_count
+            _pcloud_upload_update_file_count
         end
     end
 
-    function __update_file_count -S
+    function _pcloud_upload_update_file_count -S
         # create direcotry which might be missing when index is incremented
         rclone mkdir pcloud:"$root/$name-$i"
         set count (rclone size --json pcloud:"$root/$name-$i" | sed -rn 's/.*count":([0-9]+).*/\1/p')
         echo -n $count > ~/.local/share/tm-pcloud/count
     end
 
-    function __move_file -S -a f
+    function _pcloud_upload_move_file -S -a f
         if test -e $f
             rclone move "$f" pcloud:"$root/$name-$i" -P
             if test $status -eq 0
@@ -59,22 +59,22 @@ function pcloud-upload -d 'Upload file to fish'
     end
 
     # main
-    __create_index_files
-    __set_index_files
+    _pcloud_upload_create_index_files
+    _pcloud_upload_set_index_files
 
     while test $count -ge $limit
         tm_printMessage "$count files already present in index $i"
         set i (math $i + 1)
         echo -n $i > ~/.local/share/tm-pcloud/current
         tm_printMessage "Try dir index $i"
-        __update_file_count
+        _pcloud_upload_update_file_count
     end
 
     tm_printMessage "Dir index is $i"
     tm_printMessage "File count is $count"
 
     for f in $argv
-        __move_file "$f"
+        _pcloud_upload_move_file "$f"
     end
 
     tm_printMessage "Final file count is $count"
@@ -85,8 +85,8 @@ function pcloud-upload -d 'Upload file to fish'
         it2attention fireworks
     end
 
-    functions -e __create_index_files
-    functions -e __set_index_files
-    functions -e __update_file_count
-    functions -e __move_file
+    functions -e _pcloud_upload_create_index_files
+    functions -e _pcloud_upload_set_index_files
+    functions -e _pcloud_upload_update_file_count
+    functions -e _pcloud_upload_move_file
 end
