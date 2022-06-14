63f14e85a487dbe489e9d44a07230abe5eba4e81
Talha <talha131@oncrashreboot.com>
2022-06-14 11:04:27 +0500

    hammerspoon: add logs and minor refactor to audio input

--- hammerspoon/audio-input.lua
+++ hammerspoon/audio-input.lua
@@ -2,28 +2,33 @@
 local modalKey = hs.hotkey.modal.new(hyper, 'M', 'Mic Management mode')
 modalKey:bind('', 'escape', function() modalKey:exit() end)
 
+local logger = hs.logger.new('audio-in', 'debug')
+
 function toggleInput(v)
-   local default = hs.audiodevice.defaultInputDevice()
-   local isMute = default:inputMuted()
+   local builtin = hs.audiodevice.findDeviceByUID('BuiltInMicrophoneDevice')
+   local isMute = builtin:inputMuted()
+   logger.d(builtin:uid(), 'state:', (isMute and 'mute' or 'unmute'))
 
    local devs = hs.audiodevice.allInputDevices()
 
    local setInputDevice = false
 
+   logger.d("set all audio input state:", (not isMute and 'mute' or 'unmute'))
    for _, d in ipairs(devs) do
        d:setInputMuted(not isMute)
+       logger.d(d:uid(), 'after set state:', (d:inputMuted() and 'mute' or 'unmute'))
 
        -- prefer external headphone
        if d:uid() == 'BuiltInHeadphoneInputDevice' then
            d:setDefaultInputDevice()
            setInputDevice = true
-           loggerInfo.i('set audio input device:', d:uid())
+           logger.i('set default audio input device:', d:uid())
        end
 
        -- if headphone not found then set default input device to built in Mic
        if not setInputDevice and d:uid() == 'BuiltInMicrophoneDevice' then
            d:setDefaultInputDevice()
-           loggerInfo.i('set audio input device:', d:uid())
+           logger.i('set default audio input:', d:uid())
        end
    end
 
