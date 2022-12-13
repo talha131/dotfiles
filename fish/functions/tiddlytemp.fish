6cd0ec4556722a4b852949b384e5aa1f274e756c
Talha <talha131@oncrashreboot.com>
2022-12-14 00:59:14 +0500

    fish: add script to create temporary tiddlywiki for testing

---
+++ fish/functions/tiddlytemp.fish
@@ -0,0 +1,12 @@
+function tiddlytemp -d "Create temporary TiddlyWiki"
+   set temp (gmktemp -d)
+   set port 9999
+   tiddlywiki $temp/temp --init server
+   and begin
+       tm_printMessage "Created temporary TiddlyWiki at $temp"
+       open http://127.0.0.1:$port
+       tiddlyserver  $temp/temp --port $port
+       tm_printMessage "Clean up $temp"
+       trash -rf $temp
+   end
+end
