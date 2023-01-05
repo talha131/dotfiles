0b3376f917432d3af58b62a184a86bc35b05ffba
Talha <talha131@oncrashreboot.com>
2023-01-05 17:21:37 +0500

    fish: rename function and add option to customize the count

--- fish/functions/jump_run-test.fish
+++ fish/functions/jump_run-test.fish
@@ -1,12 +1,26 @@
-function jump_run-test-20 -d 'Run a Golang test 20 times'
+function jump_run-test -d 'Run a Golang test multiple times'
+  set $_flag_count 20
+  argparse 'n/count=' -- $argv
+  or return
+
+  # Print help
+  if set --query _flag_help
+    printf "Usage: jump_run-test [OPTIONS]\n\n"
+    printf "Options:\n"
+    printf "  -h/--help       Prints help and exits\n"
+    printf "  -n/--count=NUM  Count (minimum 1, default 10)"
+    return 0
+  end
+
+  set --query _flag_count; or set --local _flag_count 10
+
   for f in $argv
-    set times 20
     # postive, negative
     set p 0
     set n 0
 
     tm_printMessage $f
-    for i in (seq 1 $times)
+    for i in (seq 1 $_flag_count)
       # print progress
       echo -n '.'
 
