9fc3b2b057a05e93655c6ce62afdd6ba3835ce17
Talha <talha131@oncrashreboot.com>
2024-04-26 20:24:14 +0500

    hammerspoon: add code to support launching chrome app

--- hammerspoon/launch-applications.lua
+++ hammerspoon/launch-applications.lua
@@ -5,22 +5,29 @@ modalKey:bind('', 'escape', function() modalKey:exit() end)
 
 local appShortCuts = {
 
-    -- Most used
-    D = 'Google Chrome',
-    Q = 'TickTick',
-    S = 'Slack',
-    W = 'iterm',
-
-    -- Less frequent
-    C = 'Visual Studio Code',
-    P = '1Password',
-    T = 'Telegram Desktop',
-    X = 'Xcode',
-    Y = 'WhatsApp',
+    D = { appName = 'Google Chrome', os = "macOS" },
+    Q = { appName = 'TickTick', os = "chromeOS" },
+    S = { appName = 'Slack', os = "macOS" },
+    W = { appName = 'iterm', os = "macOS" },
+    C = { appName = 'Visual Studio Code', os = "macOS" },
+    P = { appName = '1Password', os = "macOS" },
+    T = { appName = 'Telegram Desktop', os = "macOS" },
+    X = { appName = 'Xcode', os = "macOS" },
+    Y = { appName = 'WhatsApp', os = "macOS" },
 }
 
 for key, app in pairs(appShortCuts) do
-
-    modalKey:bind('', key, 'Launching '..app, function() hs.application.launchOrFocus(app) end, function() modalKey:exit() end)
+  modalKey:bind('', key, 'Launching ' .. app.appName, 
+    function()
+      if app.os == 'macOS' then
+        hs.application.launchOrFocus(app.appName)
+      elseif app.os == 'chromeOS' then
+        hs.application.launchOrFocus(os.getenv('HOME') .. '/Applications/Chrome Apps.localized/' .. app.appName .. '.app')
+      end
+    end, 
+    function() 
+      modalKey:exit() 
+    end
+  )
 end
 
