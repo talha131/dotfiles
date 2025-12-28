68923bb641e9138924160121bbd0db086bac3377
Talha <talha131@gmail.com>
2025-12-28 23:05:38 +0500

    Change Neovim command to open files in VS Code

--- fish/functions/on.fish
+++ fish/functions/on.fish
@@ -3,6 +3,6 @@ function on -d 'Open a Neovim session'
   set n (fd . -t f $folder | sed 's/.*\///' | choose)
 
   if test -f "$folder/$n"
-    nvim -S "$folder/$n"
+    code "$folder/$n"
   end
 end
